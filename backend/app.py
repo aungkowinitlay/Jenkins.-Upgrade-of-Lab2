aa6ef9aba7ade91fade7d29defe8512a4da2db4e
aungkowinitlay <aungkowin.osit@gmail.com>
2025-05-15 22:15:01 +0630

    IP Address Change

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://192.168.1.8"}})
+CORS(app, resources={r"/api/*": {"origins": "http://192.168.1.7"}})
 
 @app.route("/api/message", methods=["GET"])
 def get_message():
